e9230b4283335ce851fee3f2087b04103792e39f
Satchel <satchelspencer@gmail.com>
2017-10-22 18:26:09 -0600

    Added python implementation for fetching OSM

---
+++ util/fetchOSM.py
@@ -0,0 +1,93 @@
+import urllib2
+import json
+import sys
+import pyproj #must install
+import base64
+import time
+import math
+
+wgs84 = pyproj.Proj(init='epsg:4326')
+webMerc = pyproj.Proj(init='epsg:3857')
+def transform(x,y):
+	return pyproj.transform(wgs84, webMerc, x, y)
+
+bottomRight = transform(-101, 38)
+topLeft = transform(-101.1, 39.1)
+
+auth = 'Basic ' + base64.b64encode(sys.argv[1])
+
+#create job
+job = {
+	'job' : {
+		'parameters' : {
+			'job_north' : topLeft[1],
+			'job_south' : bottomRight[1],
+			'job_east' : bottomRight[0],
+			'job_west' : topLeft[0],
+			'job_datum_projection' : 'Native',
+			'job_file_format' : 'GEOJSON',
+			'job_layers' : 'Building'
+		},
+		'dataset_token' : 'e0dd1ef6-df94-4e7f-a95f-f48327ba3467',
+		'layers' : ['Building'],
+		'content_license_acceptance' : True
+	}
+}
+
+jobRequest = urllib2.Request("https://market.trimbledata.com/jobs.json", json.dumps(job))
+jobRequest.add_header('Content-Type', 'application/json')
+jobRequest.add_header('Authorization', auth)
+res = urllib2.urlopen(jobRequest)
+jobData = json.loads(res.read())
+jobToken = jobData['job']['token']
+
+print 'created job %s' % jobToken
+
+#order job
+order = {
+	'order' : {
+		'payment_method_id' : None
+	}
+}
+
+orderRequest = urllib2.Request('https://market.trimbledata.com/jobs/%s/process_job.json' % jobToken, json.dumps(order))
+orderRequest.add_header('Content-Type', 'application/json')
+orderRequest.add_header('Authorization', auth)
+orderRequest.get_method = lambda: 'PUT'
+res = urllib2.urlopen(orderRequest)
+orderData = json.loads(res.read())
+print 'ordered job'
+
+# wait for job to process
+ready = False
+checkData = None
+i=2
+while ready == False:
+	checkRequest = urllib2.Request('https://market.trimbledata.com/jobs/%s.json' % jobToken)
+	checkRequest.add_header('Authorization', auth)
+	res = urllib2.urlopen(checkRequest)
+	checkData = json.loads(res.read())
+	status = checkData['job']['status']
+	if status == 'Ready for Download':
+		ready = True
+	else: 
+		delay = 5*math.log(i-1)
+		print 'status: %s. waiting...' % status
+		i = i+1
+		time.sleep(delay)
+
+print 'job ready'
+
+# fetch dl link
+downloadApiUrl = checkData['job']['download_api_url']
+dlUrlRequest = urllib2.Request(downloadApiUrl)
+dlUrlRequest.add_header('Authorization', auth)
+res = urllib2.urlopen(dlUrlRequest)
+dlUrlData = json.loads(res.read())
+downloadUrl = dlUrlData[0]['url']
+
+#download it
+resp = urllib2.urlopen(downloadUrl)
+file = open("%s.zip" % jobToken, 'w')
+file.write(resp.read())
+
