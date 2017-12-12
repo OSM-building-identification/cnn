3810eea8dd24e14ac7ea70983e010cd2038b9cb7
Ubuntu <ubuntu@ip-172-31-18-55.us-west-2.compute.internal>
2017-12-12 18:09:44 +0000

    added simple http auth

--- server/index.py
+++ server/index.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask import jsonify
 from flask import send_file
+from flask_httpauth import HTTPBasicAuth
 from flask_cors import CORS
 from cred import *
 import psycopg2
 
 app = Flask(__name__)
 CORS(app)
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_pw(username):
+    return CRED['http']['pass']
 
 
 conn = psycopg2.connect(
@@ -19,24 +25,28 @@ cur = conn.cursor()
 
 
 
-@app.route("/")
+@app.route("/training_tiles")
+@auth.login_required
 def all():
 	cur.execute('select x,y from training_tiles;')
 	a=cur.fetchall()
 	return jsonify(a)
 
 @app.route("/t/<int:x>/<int:y>")
+@auth.login_required
 def tile(x, y):
 	path = '../../tiles/%s_%s.jpg' % (x,y)
 	return send_file(path, mimetype='image/jpg')
 
 @app.route("/unverified")
+@auth.login_required
 def unver():
 	cur.execute('select x,y from training_tiles where verified=false;')
 	a=cur.fetchall()
 	return jsonify(a)
 
 @app.route("/verify/<int:x>/<int:y>/<string:building>")
+@auth.login_required
 def verify(x, y, building):
 	is_building = building=='true'
 	print (x,y,is_building)
@@ -45,6 +55,7 @@ def verify(x, y, building):
 	return ""
 
 @app.route("/predictions")
+@auth.login_required
 def preds():
 	cur.execute('select x,y from predictions where has_building=true;')
 	a=cur.fetchall()
