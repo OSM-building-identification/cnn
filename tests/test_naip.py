75f62308778983e5feb038771db8809d27f51331
Nick Montoya <nimo0295@colorado.edu>
2018-02-15 16:09:09 -0700

    More tests for the naip functions

--- tests/test_naip.py
+++ tests/test_naip.py
@@ -25,5 +25,19 @@ class TestNAIP(unittest2.TestCase):
         self.assertAlmostEqual(z[0], -207.46307373, places=6)
         self.assertAlmostEqual(z[1], 82.01565745, places=6)
 
+    def test_deg2tile(self):
+        x = naip.deg2tile(-179.72534179, 85.02737824, 17)
+        y = naip.deg2tile(50, 50, 17)
+        z = naip.deg2tile(-50, -50, 17)
+
+        self.assertEqual(x[0], 100)
+        self.assertEqual(x[1], 100)
+
+        self.assertEqual(y[0], 83740)
+        self.assertEqual(y[1], 44452)
+
+        self.assertEqual(z[0], 47331)
+        self.assertEqual(z[1], 86619)
+
 if __name__=='__main__':
     unittest2.main()
