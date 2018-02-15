a7515a9ed8b6bc461155b6b33149f4a6a591231e
ryryfasch <ryan.fasching@colorado.edu>
2018-02-15 16:40:42 -0700

    test suite update for getQuads

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -10,12 +10,12 @@ class testScan(unittest2.TestCase):
         pass
 
     def test_getQuads(self):
-        x = [65536, 65171, 65900, 65536]
-        box1 = scan.getQuads(x)
+
+        box1 = scan.getQuads([65536, 65171, 65900, 65536])
         #box2 = scan.getQuads()
         #box3 = scan.getQuads()
 
-        self.assertAlmostEqual(box1[0], [(65536, 65171, 65718.0, 65353.0), (65718.0, 65171, 65900, 65353.0), (65536, 65353.0, 65718.0, 65536), (65718.0, 65353.0, 65900, 65536)], places=1)
+        self.assertAlmostEqual(box1, [(65536, 65171, 65718.0, 65353.0), (65718.0, 65171, 65900, 65353.0), (65536, 65353.0, 65718.0, 65536), (65718.0, 65353.0, 65900, 65536)], places=1)
 
 if __name__ == '__main__':
     unittest2.main()
